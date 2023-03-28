df934e3e34e0b43ac6fad38e768cad678295eace
Dimitar Ivanov <dimitarivanovit@gmail.com>
2023-03-28 16:42:28 +0300

    minor strapi middleware change

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,6 +14,7 @@ module.exports = [
       },
     },
   },
+  'strapi::poweredBy',
   {
     name: 'strapi::cors',
     config: {
@@ -24,7 +25,6 @@ module.exports = [
       keepHeaderOnError: true,
     }
   },
-  'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
