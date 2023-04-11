55c2ac26014b4a8736e03e337d2a02c0ebed5a8d
Dimitar Ivanov <dimitarivanovit@gmail.com>
2023-04-11 16:51:01 +0300

    Update index.js
    Added hook event after user register to add customer stripe id

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const stripe = require('stripe')('sk_test_MwaUHxwoYwjncVDQNi1yUGSX008jSPXA4b');
+
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -7,7 +9,7 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register(/*{ strapi }*/) { },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +18,26 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  // bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    strapi.db.lifecycles.subscribe({
+      models: ['plugin::users-permissions.user'],
+
+      async afterCreate(user) {
+        // console.log(user.result.username);
+
+        stripe.customers.create({
+          email: user.result.email,
+          description: user.result.username,
+        }).then(customer => {
+          strapi.query('plugin::users-permissions.user').update({
+            where: { id: user.result.id },
+            data: {
+              stripe_customer_id: customer.id,
+            }
+          });
+        });
+      },
+    });
+  },
 };
