1b3b5323d55ab9e1af40219c5c8d0e50c4b1f1bd
Dimitar Ivanov <dimitarivanovit@gmail.com>
2023-04-16 00:59:00 +0300

    Update request-signature.js
    test

--- src/middlewares/request-signature.js
+++ src/middlewares/request-signature.js
@@ -15,31 +15,31 @@ module.exports = () => {
 
 
         // const expectedSignature = '56ac656c7f932c5b775be28949e90af9a2356eae2826539f10ab6526a0eec762';
-        if (!ctx.request['url'].includes("admin") && !ctx.request['url'].includes('content-manager')) {
-            if (ctx.request['url'].includes("orders") || ctx.request['url'].includes("tables") || ctx.request['url'].includes("categories") || ctx.request['url'].includes("items")) {
-                const generatedSignature = generateSignature(
-                    'fUjXn2r5u8x/A?D(G-KaPdSgVkYp3s6v',
-                    ctx.request['method'],
-                    ctx.request['url'],
-                    ctx.request.header['x-app-timestamp'],
-                    ctx.request.header['x-app-nonce']
-                );
+        // if (!ctx.request['url'].includes("admin") && !ctx.request['url'].includes('content-manager')) {
+        //     if (ctx.request['url'].includes("orders") || ctx.request['url'].includes("tables") || ctx.request['url'].includes("categories") || ctx.request['url'].includes("items")) {
+        //         const generatedSignature = generateSignature(
+        //             'fUjXn2r5u8x/A?D(G-KaPdSgVkYp3s6v',
+        //             ctx.request['method'],
+        //             ctx.request['url'],
+        //             ctx.request.header['x-app-timestamp'],
+        //             ctx.request.header['x-app-nonce']
+        //         );
 
-                // console.log(generatedSignature);
+        //         // console.log(generatedSignature);
 
-                // console.log(ctx.request.header['x-app-signature']);
-                const singaturesAreSame = (generatedSignature === ctx.request.header['x-app-signature']) || ctx.request.header['x-app-signature'] === 'test';
-                //console.log(singaturesAreSame);
-                //console.log(generatedSignature !== ctx.request.header['x-app-signature']);
+        //         // console.log(ctx.request.header['x-app-signature']);
+        //         const singaturesAreSame = (generatedSignature === ctx.request.header['x-app-signature']) || ctx.request.header['x-app-signature'] === 'test';
+        //         //console.log(singaturesAreSame);
+        //         //console.log(generatedSignature !== ctx.request.header['x-app-signature']);
 
-                if (singaturesAreSame === false) {
-                    console.log("Wrong signature");
-                    console.log(generatedSignature);
-                    console.log(ctx.request.header['x-app-signature']);
-                    return {};
-                }
-            }
-        }
+        //         if (singaturesAreSame === false) {
+        //             console.log("Wrong signature");
+        //             console.log(generatedSignature);
+        //             console.log(ctx.request.header['x-app-signature']);
+        //             return {};
+        //         }
+        //     }
+        // }
 
 
         // console.log(expectedSignature == generatedSignature);
